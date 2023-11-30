e6e1875093f07f8c682fbd281edfa32ccd75f952
Ping Mamueng <ping.mamueng@softwarfare.com>
2023-11-30 13:33:12 -0600

    fix port_forward.sh

--- swis_dev/port_forward.sh
+++ swis_dev/port_forward.sh
@@ -1,1 +1,9 @@
-kubectl port-forward service/go-self-service-ui-service 4455:4455 -n orypoc & kubectl port-forward service/hydra-admin 4445:4445 -n orypoc & kubectl port-forward service/hydra-public 4444:4444 -n orypoc & kubectl port-forward service/kratos-admin 4434:4434 -n orypoc & kubectl port-forward service/kratos-public 4433:4433 -n orypoc & kubectl port-forward service/mailslurper 4436:4436 -n orypoc & kubectl port-forward service/mailslurper 4437:4437 -n orypoc
+kubectl port-forward service/go-self-service-ui-service 4485:4485 -n orypoc & \
+kubectl port-forward service/hydra-admin 4445:4445 -n orypoc & \
+kubectl port-forward service/hydra-public 4444:4444 -n orypoc & \
+kubectl port-forward service/kratos-admin 4434:4434 -n orypoc & \
+kubectl port-forward service/kratos-public 4433:4433 -n orypoc & \
+kubectl port-forward service/oathkeeper-api 4456:4456 -n orypoc & \
+kubectl port-forward service/oathkeeper-proxy 4455:4455 -n orypoc &\
+kubectl port-forward service/mailslurper 4436:4436 -n orypoc & \
+kubectl port-forward service/mailslurper 4437:4437 -n orypoc
