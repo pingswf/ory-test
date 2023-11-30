42b8f147ba57117745abd42372d39b6b8e8e0bde
Ping Mamueng <ping.mamueng@softwarfare.com>
2023-11-30 13:11:43 -0600

    Modify start_ory.sh script to accept parameters

--- local_cluster/start_ory.sh
+++ local_cluster/start_ory.sh
@@ -2,6 +2,6 @@ kubectl apply -f self-service-ui/deployment.yml -n orypoc
 kubectl apply -f self-service-ui/service.yml -n orypoc
 kubectl apply -f mailslurper/deployment.yml -n orypoc
 kubectl apply -f mailslurper/service.yml -n orypoc
-helm install kratos -f kratos/values.yml ory/kratos -n orypoc
-helm install hydra -f hydra/values.yml ory/hydra -n orypoc
+helm install kratos -f kratos/values.yml ory/kratos -n orypoc --set "kratos.config.dsn=postgres://$1:$2@$3/kratos"
+helm install hydra -f hydra/values.yml ory/hydra -n orypoc --set "hydra.config.dsn=postgres://$1:$2@$3/hydra"
 helm install oathkeeper -f oathkeeper/values.yml ory/oathkeeper -n orypoc
